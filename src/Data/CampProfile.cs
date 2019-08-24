0b090a43d900a55e886d2ee014c71c258f2ee88b
Paweł Żółtaniecki <zolty1902@gmail.com>
2019-08-24 21:09:31 +0200

    Add mapping for Location fields for CampModel

--- src/Data/CampProfile.cs
+++ src/Data/CampProfile.cs
@@ -7,7 +7,15 @@ namespace CoreCodeCamp.Data
     {
         public CampProfile()
         {
-            this.CreateMap<Camp, CampModel>();
+            this.CreateMap<Camp, CampModel>()
+                .ForMember(campModel => campModel.Venue, o => o.MapFrom(camp => camp.Location.VenueName))
+                .ForMember(campModel => campModel.Address1, o => o.MapFrom(camp => camp.Location.Address1))
+                .ForMember(campModel => campModel.Address2, o => o.MapFrom(camp => camp.Location.Address2))
+                .ForMember(campModel => campModel.Address3, o => o.MapFrom(camp => camp.Location.Address3))
+                .ForMember(campModel => campModel.CityTown, o => o.MapFrom(camp => camp.Location.CityTown))
+                .ForMember(campModel => campModel.StateProvince, o => o.MapFrom(camp => camp.Location.StateProvince))
+                .ForMember(campModel => campModel.PostalCode, o => o.MapFrom(camp => camp.Location.PostalCode))
+                .ForMember(campModel => campModel.Country, o => o.MapFrom(camp => camp.Location.Country));
         }
     }
 }
--- src/Models/CampModel.cs
+++ src/Models/CampModel.cs
@@ -8,5 +8,14 @@
         public string Moniker { get; set; }
         public DateTime EventDate { get; set; }
         public int Length { get; set; } = 1;
+
+        public string Venue { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Address3 { get; set; }
+        public string CityTown { get; set; }
+        public string StateProvince { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
     }
 }
