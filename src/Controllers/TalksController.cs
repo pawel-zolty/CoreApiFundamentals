7b1f1b14bd812a1d01ebb4b9f9d4043e54140569
Paweł Żółtaniecki <zolty1902@gmail.com>
2019-08-25 00:34:41 +0200

    Add TalksController and Get action for all camp's  talks

---
+++ src/Controllers/TalksController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/camps/{moniker}/{controller}")]
+    [ApiController]
+    public class TalksController : ControllerBase
+    {
+        private readonly ICampRepository _campRepository;
+        private readonly IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
+
+        public TalksController(ICampRepository campRepository, IMapper mapper, LinkGenerator linkGenerator)
+        {
+            this._campRepository = campRepository;
+            this._mapper = mapper;
+            this._linkGenerator = linkGenerator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<TalkModel[]>> Get(string moniker, bool includeSpeakers = false)
+        {
+            try
+            {
+                var results = await _campRepository.GetTalksByMonikerAsync(moniker, includeSpeakers);
+
+                return _mapper.Map<TalkModel[]>(results);
+            }
+            catch (Exception e)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+        }
+    }
+}
