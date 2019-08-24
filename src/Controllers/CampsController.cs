87b47a8edcef89f4c2639d96e4d49413d00d8b9e
Paweł Żółtaniecki <zolty1902@gmail.com>
2019-08-24 20:20:01 +0200

    add CampsController with first action GetCamps

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,38 @@
+﻿using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository _campRepository;
+
+        public CampsController(ICampRepository campRepository)
+        {
+            this._campRepository = campRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Camp[]>> GetCamps()
+        {
+            try
+            {
+                var results = await _campRepository.GetAllCampsAsync();
+
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+            
+        }
+    }
+}
